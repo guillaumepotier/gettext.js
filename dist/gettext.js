533d8ea0d56a546af90a7a299341ee16d5dfa6ab
Andrey Khoronko <andrey.khoronko@gmail.com>
2016-02-16 13:18:01 +0300

    use passed to getPluralFunc _plural_form

--- dist/gettext.js
+++ dist/gettext.js
@@ -53,14 +53,14 @@
         // plural forms list available here http://localization-guide.readthedocs.org/en/latest/l10n/pluralforms.html
         var pf_re = new RegExp('^\\s*nplurals\\s*=\\s*[0-9]+\\s*;\\s*plural\\s*=\\s*(?:\\s|[-\\?\\|&=!<>+*/%:;n0-9_\(\)])+');
 
-        if (!pf_re.test(_plural_forms[_locale]))
-          throw new Error(strfmt('The plural form "%1" is not valid', _plural_forms[_locale]));
+        if (!pf_re.test(plural_form))
+          throw new Error(strfmt('The plural form "%1" is not valid', plural_form));
 
         // Careful here, this is a hidden eval() equivalent..
         // Risk should be reasonable though since we test the plural_form through regex before
         // taken from https://github.com/Orange-OpenSource/gettext.js/blob/master/lib.gettext.js
         // TODO: should test if https://github.com/soney/jsep present and use it if so
-        return new Function("n", 'var plural, nplurals; '+ _plural_forms[_locale] +' return { nplural: nplurals, plural: (plural === true ? 1 : (plural ? plural : 0)) };');
+        return new Function("n", 'var plural, nplurals; '+ plural_form +' return { nplural: nplurals, plural: (plural === true ? 1 : (plural ? plural : 0)) };');
       };
 
       // Proper translation function that handle plurals and directives
