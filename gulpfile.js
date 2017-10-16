08730ab5492220a99ee40b3b5b5513df7d710975
Andre Franciscato Paggi <andre.paggi@corp.nova>
2017-10-16 16:11:12 -0200

    Minify with minimun comments

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,6 @@ gulp.task('scripts', function () {
     return gulp.src('lib/gettext.js')
         .pipe(gulp.dest('dist'))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(uglify({output: { comments: 'all' }}))
+        .pipe(uglify({output: { comments: 'some' }}))
         .pipe(gulp.dest('dist'));
 });
