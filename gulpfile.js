a19b9fd1d59391f087b3d8dac9a80d8f9f23d5f1
troggendorf <git@schrotie.de>
2019-06-12 21:35:55 +0200

    Changed build to produce various specialized outputs for different module systems.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,35 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+const {dest, parallel, series, src} = require('gulp');
+const del      = require('del');
+const rename   = require('gulp-rename');
+const rollup   = require('gulp-better-rollup');
+const uglify   = require('gulp-uglify-es').default;
 
-gulp.task('default', function () {
-    gulp.start('scripts');
-});
+const BUILD_DIR = 'dist';
 
-gulp.task('scripts', function () {
-    return gulp.src('lib/gettext.js')
-        .pipe(gulp.dest('dist'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(uglify({output: { comments: 'some' }}))
-        .pipe(gulp.dest('dist'));
-});
+const cleanUp = () => del([BUILD_DIR]);
+
+
+const global = () =>	src(`lib/gettext.global.js`)
+	.pipe(rollup({format: 'iife'}))
+	.pipe(dest(BUILD_DIR));
+
+const pack = format => () => src(`lib/gettext.js`)
+	.pipe(rollup({format}))
+	.pipe(rename({suffix: `.${format}`}))
+	.pipe(dest(BUILD_DIR));
+
+const minify = name => () => src(`./${BUILD_DIR}/gettext.${name}.js`)
+	.pipe(rename({suffix: '.min'}))
+   .pipe(uglify({output: { comments: 'some' }}))
+	.pipe(dest(BUILD_DIR));
+
+
+const formats = ['amd', 'cjs', 'esm', 'iife', 'system', 'umd'];
+
+const build = series(
+	parallel(global, ...formats.map(pack)),
+	['global'].concat(formats).map(minify)
+);
+
+
+exports.default  = series(cleanUp, build);
